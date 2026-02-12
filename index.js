990eaac3754a462d807bbbbe8825eb1e7bae9d72
NinjafR7 <ninjafrpro@gmail.com>
2026-02-12 17:14:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+
 const app = express();
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", ()=>console.log("Bot Ready"));
+client.login(process.env.TOKEN);
 
 app.use(express.static("public"));
 
-app.get("/api/lockdown", (req,res)=>{
-    res.send("Lockdown signal sent to bot");
+/* ===== LOCKDOWN ===== */
+app.get("/api/lockdown", async (req,res)=>{
+    const guild = client.guilds.cache.get("1468970047937581108");
+
+    guild.channels.cache.forEach(ch=>{
+        ch.permissionOverwrites.edit(guild.roles.everyone,{ SendMessages:false }).catch(()=>{});
+    });
+
+    res.send("Server locked");
 });
 
-app.listen(3000, ()=>{
-    console.log("Nexus panel running");
+/* ===== UNLOCK ===== */
+app.get("/api/unlock", async (req,res)=>{
+    const guild = client.guilds.cache.get("1468970047937581108");
+
+    guild.channels.cache.forEach(ch=>{
+        ch.permissionOverwrites.edit(guild.roles.everyone,{ SendMessages:true }).catch(()=>{});
+    });
+
+    res.send("Server unlocked");
 });
+
+app.listen(3000, ()=>console.log("Panel running"));
